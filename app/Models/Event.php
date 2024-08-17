3626572083c6cca41012dfe03314fc7156aa8075
Abbey <abbeysantos1210@gmail.com>
2024-08-17 22:08:19 +0800

    chore: add event and gallery model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'EventID';
+
+    protected $fillable = [
+        'EventImage',
+        'EventName',
+        'EventDescription',
+        'Date',
+        'StartTime',
+        'EndTime',
+        'Location',
+        'Link',
+        'Status',
+        'UserID',
+    ];
+
+    protected $foreignKey = ['Status', 'UserID'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id');
+    }
 }
